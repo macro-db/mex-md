47c7cb45edd341ee676672283688733040b29cdb
AlvaroMaza <alvaromazamontesinos@gmail.com>
2024-03-12 12:52:51 +0100

    x13arima test

---
+++ src/x13arima.py
@@ -0,0 +1,25 @@
+import statsmodels.api as sm
+import pandas as pd
+
+
+""" df = pd.read_csv('data/2024_03_11.csv')
+
+idx = pd.to_datetime(df["fecha"], format = "%d/%m/%Y")
+df.set_index(idx, inplace=True)
+
+series = df['SL11298'][400:-1]
+
+x13 = sm.tsa.x13_arima_analysis(series, x12path = "C:/Users/alvar/Desktop/BDE/banxico-md/x13as")
+ """
+
+dta = sm.datasets.co2.load_pandas().data
+dta.co2.interpolate(inplace=True)
+dta = dta.resample('M').sum()
+
+
+x13 = sm.tsa.x13_arima_analysis(dta.co2, x12path = "C:/Users/alvar/Desktop/BDE/banxico-md/x13as")
+print(x13.trend)
+
+""" plt.plot(dta.co2)
+plt.plot(x13.trend)
+plt.show() """
