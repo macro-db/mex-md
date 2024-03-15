4dd8fe63d7c0b45eeef49079dcaab6d3839d7c12
AlvaroMaza <alvaromazamontesinos@gmail.com>
2024-03-15 13:49:22 +0100

    pandas pipeline

--- src/df_utils.py
+++ src/df_utils.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import numpy as np
+import statsmodels.api as sm
 
 from datetime import date
+
+from utils import read_yaml
 
 
 def order(df):
@@ -25,7 +29,7 @@ def order(df):
     return df
 
 
-def load(df, prefix='', index=False):
+def save(df, prefix='', index=False):
     """
     Stores the DataFrame in a CSV file with today's date appended to the filename.
 
@@ -36,3 +40,125 @@ def load(df, prefix='', index=False):
     """
     today = date.today().strftime("%Y_%m_%d")
     df.to_csv(f'data/{prefix+today}.csv', index=index)
+
+    return df
+
+
+def set_date_index(df):
+    """
+    Preprocess the DataFrame by converting the 'fecha' column to datetime and setting it as index.
+
+    Parameters:
+        df (DataFrame): Input DataFrame.
+
+    Returns:
+        DataFrame: Preprocessed DataFrame with date index.
+    """
+    df['fecha'] = pd.to_datetime(df['fecha'], format='%d/%m/%Y')
+    df.set_index('fecha', inplace=True)
+    return df
+
+
+def slice_df_from_date(df, start_date):
+    """
+    Slice the DataFrame to get data from the specified start date onwards.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame to be sliced.
+    start_date (str): The start date from which to slice the DataFrame (format: 'YYYY-MM-DD').
+
+    Returns:
+    pandas.DataFrame: Sliced DataFrame starting from the specified start date.
+    """
+    return df[start_date:]
+
+
+def stationarize_panel(df, series):
+    """
+    Stationarize the specified series in the DataFrame using X13-ARIMA analysis.
+
+    Parameters:
+        df (DataFrame): Input DataFrame.
+        series (list): List of column names to stationarize.
+
+    Returns:
+        None
+    """
+    # Make a copy of the original DataFrame
+    df_stationarized = df.copy()
+
+    # Apply x13_arima_analysis to each series
+    for column in series:
+        try:
+            # Perform X13-ARIMA analysis
+            x13_result = sm.tsa.x13_arima_analysis(df[column], x12path="x13as")
+        except Exception as e:
+            print(f'Error on X13-ARIMA with series {column}: ', e)
+        else:
+            # Replace the original series with the trend component
+            df_stationarized.loc[:, column] = x13_result.trend
+
+    return df_stationarized
+
+
+def remove_outliers(df, column_names, threshold=1.5):
+    """
+    Removes outliers from specified columns of a DataFrame using the IQR method.
+
+    Parameters:
+        df (DataFrame): Input DataFrame.
+        column_names (list): List of column names to detect outliers from.
+        threshold (float): Multiplier for the IQR to determine outliers. Default is 1.5.
+
+    Returns:
+        DataFrame: DataFrame with outliers removed.
+    """
+    # Make a copy of the original DataFrame
+    df_filtered = df.copy()
+    
+    for column_name in column_names:
+        # Calculate quartiles and IQR
+        q1 = df[column_name].quantile(0.25)
+        q3 = df[column_name].quantile(0.75)
+        IQR = q3 - q1
+
+        # Calculate lower and upper bounds for outlier detection
+        lower_bound = q1 - threshold * IQR
+        upper_bound = q3 + threshold * IQR
+
+        # Filter DataFrame to remove outliers
+        df_filtered = df_filtered[(df_filtered[column_name] >= lower_bound) & (df_filtered[column_name] <= upper_bound)]
+    
+    return df_filtered
+    
+
+def transformation(tcode, x):
+    small = 1e-10  # Defined small value for logs
+
+    if tcode == 1:  # Level (no transformation)
+        y = x
+
+    elif tcode == 2:  # First difference
+        y = np.diff(x, axis=0)
+
+    elif tcode == 3:  # Second difference
+        y = np.diff(x, n=2, axis=0)
+
+    elif tcode == 4:  # Natural log
+        y = np.where(np.min(x) < small, np.nan, np.log(x))
+
+    else:
+        raise ValueError("Invalid transformation code")
+
+    return y
+
+def apply_transformations(df):
+    settings = read_yaml("src/settings_test.yaml")
+    series = {key: value for key, value in settings.items() if key != 'base_url'}
+
+    df_transformed = df.copy()
+
+    for serie, info in series.items():
+        df_transformed[serie] = df_transformed[serie].apply(lambda x: transformation(info['transformation'], x))
+    
+    return df_transformed
--- src/main.py
+++ src/main.py
@@ -1,30 +1,19 @@
 import pandas as pd
 from extract import extract
-from df_utils import order, load
-from df_transformations import stationarize_panel, slice_df_from_date, remove_outliers
+from df_utils import order, save, stationarize_panel, slice_df_from_date, remove_outliers, apply_transformations, set_date_index
 
 if __name__ == "__main__":
     # Extract the data from the data source API
     df = extract()
     
-    # Order the data by date
-    df = order(df)
-    
-    # Load the raw output to a csv file
-    load(df)
-
-    # Convert 'fecha' column to datetime and set it as index
-    df['fecha'] = pd.to_datetime(df['fecha'], format='%d/%m/%Y')
-    df.set_index('fecha', inplace=True)
-
-    # Slice the DataFrame to get data from 2000 onwards
-    df = slice_df_from_date(df, start_date='2000-01-01')
-
-    # Apply x13_arima_analysis to each series
-    stationarize_panel(df, ['SP74663', 'SF4782'])
-    
-    # Remove outliers
-    df = remove_outliers(df, ['SP74663', 'SF4782'])
-    
-    # Load balanced data to a csv file with index
-    load(df, prefix='balanced_', index=True)
+    print(df)
+    (
+        df.pipe(order)
+        .pipe(save)
+        .pipe(set_date_index)
+        .pipe(slice_df_from_date, start_date='2000-01-01')
+        .pipe(stationarize_panel, ['SP74663', 'SF4782'])
+        .pipe(remove_outliers, ['SP74663', 'SF4782'])
+        .pipe(apply_transformations)
+        .pipe(save, prefix='balanced_', index=True)
+    )
